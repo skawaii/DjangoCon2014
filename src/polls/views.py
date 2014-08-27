705a549e1990d8e2d7b4fa52549ccb3eadd176a7
Ramon Maria Gallart <ramon.gallart@docusign.com>
2014-08-27 14:44:55 -0700

    Views and templates

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Poll
 
-# Create your views here.
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_poll_list': latest_poll_list,
+    }
+
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request,
+        'polls/detail.html',
+        {
+            'poll': poll,
+            'choices': poll.choice_set.all(),
+        })
+
+
+def results(request, poll_id):
+    return HttpResponse("You are looking at the results of poll %s." % poll_id)
+
+
+def vote(request, poll_id):
+    return HttpResponse("You are voting on poll %s." % poll_id)
