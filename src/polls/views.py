2c5fe99f88d8ebe105b9f2fa8311c38855de7d37
Ramon Maria Gallart <ramon.gallart@docusign.com>
2014-08-27 15:44:13 -0700

    Forms and generic views

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,29 +1,41 @@
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Poll
+from django.views import generic
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_poll_list': latest_poll_list,
-    }
-
-    return render(request, 'polls/index.html', context)
+from .models import Poll, Choice
 
 
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request,
-        'polls/detail.html',
-        {
-            'poll': poll,
-            'choices': poll.choice_set.all(),
-        })
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
 
 
-def results(request, poll_id):
-    return HttpResponse("You are looking at the results of poll %s." % poll_id)
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 
 def vote(request, poll_id):
-    return HttpResponse("You are voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        print "The choice: %s" % request.POST['choice']
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',
+            {
+                'poll': p,
+                'error_message': "You didn't select a choice.",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id, )))
