214728bd14e37a0b92c127b29c74eaa9a09dbeb8
Ramon Maria Gallart <ramon.gallart@docusign.com>
2014-08-28 20:57:37 -0700

    Added static folder and static files collection to staging recipe

--- site-cookbooks/poll-app-python/recipes/staging.rb
+++ site-cookbooks/poll-app-python/recipes/staging.rb
@@ -10,8 +10,10 @@
 # INSTALLING PYTHON STUFF
 bash "install_requirements-dev.txt" do
   user "vagrant"
-  cwd "/vagrant/"
+  cwd "/vagrant/src/"
   code <<-EOH
   . /home/vagrant/polls_ve/bin/activate && pip install -r requirements/requirements-staging.txt
+  mkdir -p static
+  . /home/vagrant/polls_ve/bin/activate && ./manage.py collectstatic -c --noinput
   EOH
 end
